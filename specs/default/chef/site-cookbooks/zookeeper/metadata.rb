d670f4146c8c9749d80aa9331422e02b0088fcc4
Andy Howard <anhoward@microsoft.com>
2018-07-30 10:24:23 -0400

    Fix license information.

--- specs/default/chef/site-cookbooks/zookeeper/metadata.rb
+++ specs/default/chef/site-cookbooks/zookeeper/metadata.rb
@@ -2,9 +2,6 @@
 # Licensed under the MIT License.
 
 name             'zookeeper'
-maintainer       'Cycle Computing LLC'
-maintainer_email 'suppport@cyclecomputing.com'
-license          'All rights reserved'
 description      'Installs/Configures zookeeper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
